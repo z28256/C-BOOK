2bcefd34a87216bb387afeafc503e2a769a81735
z28256 <18836930989@163.com>
2022-10-23 16:04:37 +0800

    test
    编程题

--- book_1/book_1/chapter6_loop.c
+++ book_1/book_1/chapter6_loop.c
@@ -516,3 +516,492 @@
 //}
 
 
+//6.16  1.存储并打印26个小写字母
+//#include <stdio.h>
+//int main(void)
+//{
+//	char al[26] = { 0 };
+//	int i = 0;
+//	for (al[i] = 'a'; al[i] < 26 + 'a'; al[i]++)
+//	{
+//		printf("%c ", al[i]);
+//	}
+//	return 0;
+//}
+
+
+//#define SIZE 26
+//#include <stdio.h>
+//int main(void)
+//{
+//	char ch[SIZE] = { 0 };
+//	int index = 0;
+//	for (index = 0; index < SIZE; index++)
+//	{
+//		ch[index] = 'a' + index;
+//		printf("%c ", ch[index]);
+//	}
+//	printf("\n");
+//	return 0;
+//}
+
+
+//2. 打印三角形
+//#include <stdio.h>
+//int main(void)
+//{
+//	int row = 0;
+//	int column = 0;
+//
+//	for (row = 1; row <= 5; row++)
+//	{
+//		for (column = 1; column <= row; column++)
+//		{
+//			printf("$ ");
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//3. 打印字母
+//#include <stdio.h>
+//int main(void)
+//{
+//	char row = 0;
+//	char column = 0;
+//
+//	for (column = 'F'; column >= 'F' - 5; column--)
+//	{
+//		for (row = 'F'; row >= column; row--)
+//		{
+//			printf("%c", row);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= 6; i++)
+//	{
+//		for (j = 0; j < i; j++)
+//		{
+//			printf("%c ", 'F' - j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//4.打印字母
+//#include <stdio.h>
+//#define ROWS 6
+//int main(void)
+//{
+//	char ch = 'A';
+//	int i = 0;
+//	int j = 0;
+//
+//	for (i = 0; i < ROWS; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%c ", ch++);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//5.打印字母
+//#include <stdio.h>
+//int main(void)
+//{
+//	char ch = 0;
+//	int i = 0;
+//	int j = 0;
+//
+//	printf("Enter the alphabet: ");
+//	scanf("%c", &ch);
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		for (j = 0; j < i; j++)
+//		{
+//			printf("%c", ch + j);
+//		}
+//		for (; j >= 0; j--)
+//		{
+//			printf("%c", ch + j);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#define ROWS 5
+//int main(void)
+//{
+//	char ch = 'A';
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < ROWS; i++)
+//	{
+//		for (j = 0; j < ROWS - i - 1; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < i; j++)
+//		{
+//			printf("%c", ch + j);
+//		}
+//		for (; j >= 0; j--)
+//		{
+//			printf("%c", ch + j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//6.打印表格，每一行打印一个整数及其平方和立方，输入表格的上下限
+//#include <stdio.h>
+//int main(void)
+//{
+//	int max = 0;
+//	int min = 0;
+//
+//	printf("Enter the max number:\n");
+//	scanf("%d", &max);
+//	printf("Enter the min number:\n");
+//	scanf("%d", &min);
+//	printf("%7s%7s%7s\n", "number", "square", "cube");
+//	for (; min <= max; min++)
+//	{
+//		printf("%5d\t%5d\t%5d\n", min, min * min, min * min * min);
+//	}
+//	return 0;
+//}
+
+
+//7.输入字符串，输出其倒序
+//#include <stdio.h>
+//#include <string.h>
+//int main(void)
+//{
+//	char ch[20] = { 0 };
+//	int i = 0;
+//
+//	printf("Enter the string: \n");
+//	scanf("%s", ch);
+//	for (i = (int)strlen(ch); i >= 0; i--)
+//	{
+//		printf("%c", ch[i]);
+//	}
+//	printf("\n");
+//
+//	return 0;
+//}
+
+
+//句子倒序
+//#include <stdio.h>
+//#include <string.h>
+//#define SIZE 40
+//int main(void)
+//{
+//	int i = 0;
+//	int index = 0;
+//	char ch[SIZE] = { 0 };
+//
+//	printf("Input a word: ");
+//	scanf("%c", &ch[index]);
+//	while (ch[index] != '\n')
+//	{
+//		index++;
+//		scanf("%c", &ch[index]);
+//	}
+//	for (index = (int)strlen(ch); index >= 0; index--)
+//	{
+//		printf("%c", ch[index]);
+//	}
+//
+//	return 0;
+//}
+
+
+//8.循环输入两个浮点数，输出两数之差除以两数乘积的结果
+//#include <stdio.h>
+//int main(void)
+//{
+//	double num1 = 0;
+//	double num2 = 0;
+//	double result = 0;
+//
+//	printf("Enter two floating numbers: \n");
+//	while (scanf("%lf%lf", &num1, &num2) == 2)
+//	{
+//		printf("Result: %.2f\n", (num1 - num2) / (num1 * num2));
+//		printf("Enter two floating numbers: (enter q to quit)\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//9.修改第8题为函数
+//#include <stdio.h>
+//double Result(double x, double y);
+//int main(void)
+//{
+//	double num1 = 0;
+//	double num2 = 0;
+//	double result = 0;
+//
+//	printf("Enter two numbers: \n");
+//	while (scanf("%lf%lf", &num1, &num2) == 2)
+//	{
+//		result = Result(num1, num2);
+//		printf("Result: %.2f\n", result);
+//		printf("Enter two numbers(enter q to quit): \n");
+//	}
+//
+//	return 0;
+//}
+//double Result(double x, double y)
+//{
+//	return (x - y) / (x * y);
+//}
+
+
+//10.输入上下限，输出上下限内所有整数的平方和
+//#include <stdio.h>
+//int main(void)
+//{
+//	int lower = 0;
+//	int upper = 0;
+//	int squares_sum = 0;
+//	int i = 0;
+//
+//	printf("Enter lower and upper integer limits: ");
+//	scanf("%d%d", &lower, &upper);
+//	while (lower < upper)
+//	{
+//		squares_sum = 0;
+//		for (i = lower; i <= upper; i++)
+//		{
+//			squares_sum += i * i;
+//		}
+//		printf("The sums of the squares from %d to %d is %d\n",
+//			lower * lower, upper * upper, squares_sum);
+//		printf("Enter next set of limits: ");
+//		scanf("%d%d", &lower, &upper);
+//	}
+//
+//	return 0;
+//}
+
+
+//11.输入8个整数，倒序打印
+//#include <stdio.h>
+//int main(void)
+//{
+//	int arr[8] = { 0 };
+//	int index = 0;
+//	for (index = 0; index < 8; index++)
+//	{
+//		scanf("%d", &arr[index]);
+//	}
+//	for (index = 7; index >= 0; index--)
+//	{
+//		printf("%d ", arr[index]);
+//	}
+//	printf("\n");
+//	return 0;
+//}
+
+
+//12.无限序列
+//1.0 + 1.0 / 2.0 + 1.0 / 3.0 + 1.0 / 4.0 + ... 发散
+//1.0 - 1.0 / 2.0 + 1.0 / 3.0 - 1.0 / 4.0 + ... 收敛
+//#include <stdio.h>
+//int main(void)
+//{
+//	double i = 0;
+//	double j = 0;
+//	double sum1 = 0;
+//	double sum2 = 0;
+//
+//	scanf("%lf", &i);
+//	while (i > 0)
+//	{
+//		j = i;
+//		sum1 = 0;
+//		sum2 = 0;
+//		for (; i > 0; i--)
+//		{
+//			sum1 += 1.0 / i;
+//		}
+//		for (; j > 0; j--)
+//		{
+//			if ((int)j % 2 == 0)
+//			{
+//				sum2 += 1.0 / j;
+//			}
+//		}
+//		printf("%f\n", sum1);
+//		printf("%f\n", sum1 - 2 * sum2);
+//		scanf("%lf", &i);
+//	}
+//
+//	return 0;
+//}
+
+
+//13. 数组8个元素，2的前8次幂，for循环设置数组元素的值，do while显示值
+//#include <stdio.h>
+//#define SIZE 8
+//int main(void)
+//{
+//	int arr[SIZE] = { 2 };
+//	int i = 0;
+//	for (i = 0; i < SIZE - 1; i++)
+//	{
+//		arr[i + 1] = 2 * arr[i];
+//	}
+//	do
+//	{
+//		printf("%d\n", arr[i]);
+//	} while (i-- > 0);
+//
+//	return 0;
+//}
+
+
+//14.两个8元素的数组，第二个数组元素是第一个数组前面的元素和
+//#include <stdio.h>
+//#define SIZE 8
+//int main(void)
+//{
+//	int arr1[SIZE] = { 0 };
+//	int arr2[SIZE] = { 0 };
+//	int i = 0; 
+//
+//	for (i = 0; i < SIZE; i++)
+//	{
+//		scanf("%d", &arr1[i]);
+//		arr2[0] = arr1[0];
+//		if (i > 0)
+//		{
+//			arr2[i] = arr1[i] + arr2[i - 1];
+//		}
+//	}
+//	for (i = 0; i < SIZE; i++)
+//	{
+//		printf("%5d ", arr1[i]);
+//	}
+//	printf("\n");
+//	for (i = 0; i < SIZE; i++)
+//	{
+//		printf("%5d ", arr2[i]);
+//	}
+//	printf("\n");
+//
+//	return 0;
+//}
+
+
+//15.倒叙打印一行输入
+//#include <stdio.h>
+//#define SIZE 255
+//int main(void)
+//{
+//	char ch[SIZE] = { 0 };
+//	int i = 0;
+//	scanf("%c", &ch[i]);
+//	while (ch[i] != '\n')
+//	{
+//		i++;
+//		scanf("%c", &ch[i]);
+//	}
+//	for (; i >= 0; i--)
+//	{
+//		printf("%c", ch[i]);
+//	}
+//	return 0;
+//}
+
+
+//16.$100 原始投资的10%或者5%的复合利息 
+//#include <stdio.h>
+//int main(void)
+//{
+//	double n1 = 100;
+//	double n2 = 100;
+//	int i = 0;
+//
+//	while (n1 >= n2)
+//	{
+//		i++;
+//		n1 += 10;
+//		n2 *= 1.05;
+//	}
+//	
+//	printf("Year: %d\n", i);
+//	printf("n1: %.2f n2: %.2f\n", n1, n2);
+//
+//	return 0;
+//}
+
+
+//17.$100万，年利率8%,每年的最后一天取出10万，多少年取完
+//#include <stdio.h>
+//int main(void)
+//{
+//	double money = 100;
+//	int i = 0;
+//
+//	while (money * 1.08 -10 >= 0)
+//	{
+//		money = money * 1.08 - 10;
+//		i++;
+//	}
+//	printf("money: %f\n", money);
+//	printf("Year = %d\n", i);
+//
+//	return 0;
+//}
+
+
+//18.起初5个朋友，第N周少N个，剩下的翻倍,直到超过邓巴数150
+//#include <stdio.h>
+//int main(void)
+//{
+//	int friends = 5;
+//	int i = 0;
+//	while (friends <= 150)
+//	{
+//		i++;
+//		friends -= i;
+//		friends *= 2;
+//	}
+//	printf("friens: %d\n", friends);
+//	printf("year: %d\n", i);
+//
+//	return 0;
+//}
