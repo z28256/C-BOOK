1c628e7a9104ecfe5b0838589801b0614530a402
z28256 <18836930989@163.com>
2023-04-17 22:02:00 +0800

    Update math.c

--- SORT/Math/math.c
+++ SORT/Math/math.c
@@ -134,3 +134,84 @@ int romanToInt(char * s)
     }
     return sum;
 }*/
+
+
+//动态规划
+
+//1.爬楼梯
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+int climbStairs(int n)
+{
+    if (n < 3)
+        return n;
+    int n1 = 1;
+    int n2 = 2;
+    n -= 2;
+    int ret = 0;
+    while (n--)
+    {
+        ret = n1 + n2;
+        n1 = n2;
+        n2 = ret;
+    }
+    return ret;
+}
+
+
+//2.买卖股票的最佳时机
+//给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+//你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。
+//返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+
+int maxProfit(int* prices, int pricesSize)
+{
+    int max = 0, min = prices[0];
+    for (int i = 1; i < pricesSize; ++i)
+    {
+        if (min > prices[i])
+            min = prices[i];
+        else if (max < prices[i] - min)
+            max = prices[i] - min;
+    }
+    return max;
+}
+
+
+//3.最大子序和
+//给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+//子数组 是数组中的一个连续部分。
+
+int maxSubArray(int* nums, int numsSize)
+{
+    int max = nums[0], pre = 0;
+    for (int i = 0; i < numsSize; ++i)
+    {
+        pre = pre + nums[i] > nums[i] ? pre + nums[i] : nums[i];
+        max = max > pre ? max : pre;
+    }
+    return max;
+}
+
+
+//4.打家劫舍
+//每间房都藏有现金，制约是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被闯入，会自动报警。
+//给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+#include <stdio.h>
+int rob(int* nums, int numsSize)
+{
+    if (nums == NULL)
+        return 0;
+    if (numsSize == 1)
+        return nums[0];
+    int first = nums[0], second = nums[1] > nums[0] ? nums[1] : nums[0];
+    for (int i = 2; i < numsSize; ++i)
+    {
+        int temp = second;
+        second = second > nums[i] + first ? second : nums[i] + first;
+        first = temp;
+    }
+    return second;
+}
